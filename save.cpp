2f16bcaaf5c9acac2565189c4091ed8836a3d3c2
KwonSunwon <KwonSunwon@users.noreply.github.com>
2019-09-18 01:30:42 +0900

    Test2

---
+++ save.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#pragma warning(disable:4996)
+
+void save(char const* fname)
+{
+	std::ifstream in(fname);
+	std::ofstream out("2019 C++ ¼ö¾÷·Ï.txt", std::ios::app);
+
+	auto now = std::chrono::system_clock::now();
+	auto time = std::chrono::system_clock::to_time_t(now);
+	std::cout << ctime(&time) << std::endl;
+
+	out << "\n\n\n";
+	out << "----------------------------------------------------------------------------" << std::endl;
+	out << ctime(&time);
+	out << "----------------------------------------------------------------------------" << std::endl;
+	out << "\n\n";
+
+	int c;
+	while ((c = in.get()) != EOF)
+		out.put(c);
+}
---
+++ save.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void save(char const* fname);
